7a6db8671405537bdd6fbdd9f7a46b49c498472f
Mariana <73724268+golangis@users.noreply.github.com>
2022-01-26 20:30:16 +0000

    Um teste para dar errado e outro para dar certo

--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -1,3 +1,8 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
 SELECT * FROM Booking;
-INSERT INTO Booking VALUES (19, 100, "Premium", "Cartão de Débito", 922500868, "2022-04-01", "2022-04-05" );
+INSERT INTO Booking VALUES (29, 100, "Premium", "Cartão de Débito", 922500868, "2022-04-01", "2022-04-05" );
+INSERT INTO Booking VALUES (30, 100, "Premium", "Cartão de Débito", 922500868, "2023-03-01", "2023-03-02" );
 SELECT * FROM Booking;
