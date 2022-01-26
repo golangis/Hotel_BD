43f56d951d7b7b010a2e81b1b5d9a4828b312345
Mariana <73724268+golangis@users.noreply.github.com>
2022-01-26 20:17:22 +0000

    Pois.. não sei que tem de novo

--- int05.sql
+++ int05.sql
@@ -7,8 +7,6 @@ PRAGMA FOREIGN_KEYS = ON;
  select Booking.room_number, Guest.name, Guest.ssn
      from Booking, Guest, Renting 
         where ('2022-04-09' between Booking.begin_date and Booking.end_date)
-            --and (Booking.room_number=86) 
                 and (Renting.guest_SSN = Guest.ssn) 
                     and (Renting.booking_id = Booking.id) 
                         and (Renting.renter = 1);
-                        --GROUP BY Booking.room_number;
