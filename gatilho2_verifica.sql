35712de2084f76c72d0fd6b277d3ac9b549bf90d
emanuelgestosa <emanuel-367@hotmail.com>
2022-01-26 20:43:00 +0000

    fix gatilho2_verifica

--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -2,7 +2,10 @@
 .headers on
 .nullvalue NULL
 
+INSERT INTO BookingType VALUES("Premium", 200);
+INSERT INTO Room VALUES(100, 2, 1, 90, 3);
+
 SELECT * FROM Booking;
 INSERT INTO Booking VALUES (29, 100, "Premium", "Cartão de Débito", 922500868, "2022-04-01", "2022-04-05" );
-INSERT INTO Booking VALUES (30, 100, "Premium", "Cartão de Débito", 922500868, "2023-03-01", "2023-03-02" );
+INSERT INTO Booking VALUES (30, 100, "Premium", "Cartão de Débito", 922500868, "2022-03-01", "2022-04-02" );
 SELECT * FROM Booking;
