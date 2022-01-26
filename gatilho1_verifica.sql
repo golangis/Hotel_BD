698720579062d57d8188e0c75c5594b64d89e533
Miguel Silva <miguelcfsilva@gmail.com>
2022-01-26 19:03:49 +0000

    Fixed Mistake on Booking Type

--- gatilho1_verifica.sql
+++ gatilho1_verifica.sql
@@ -1,4 +1,7 @@
-INSERT INTO Booking VALUES (21, 100, "Plano D' Ouro", "Cartão de Débito", 922005978, "2022-04-07", "2022-04-18");
+INSERT INTO BookingType (name, extra_costs) VALUES("Plano D' Prata", 200);
+
+INSERT INTO Booking VALUES (21, 100, "Plano D' Prata", "Cartão de Débito", 922005978, "2022-04-07", "2022-04-18");
+
 
 INSERT INTO Guest (SSN, name, address, birthdate) VALUES(123678990, 'Crica Aveiro', "1234-5678, nº7 Rua do Cristiano, Madrid, Espanha", '2001-02-05' );
 INSERT INTO Guest (SSN, name, address, birthdate) VALUES(569477790, 'Okeyama Pedreiro', "9876-3567, nº29 Rua das Camélias, Lisboa, Portugal", '1997-05-04' );
