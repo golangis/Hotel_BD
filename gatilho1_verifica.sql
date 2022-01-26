157fe040e4f3d286a7d760f26ca021ab991526a0
Miguel Silva <miguelcfsilva@gmail.com>
2022-01-26 18:56:21 +0000

    Added Checking Query

---
+++ gatilho1_verifica.sql
@@ -0,0 +1,5 @@
+INSERT INTO Renting VALUES (1,123498700, 20);
+INSERT INTO Renting VALUES (0,890900023, 20);
+Select * from Renting where booking_id = 20;
+INSERT INTO Renting VALUES (1,568887700, 20);
+Select * from Renting where booking_id = 20;
